7698b88cf4afebf6ed35358823b88fa96dd0e09b
mac <mac@192.168.0.100>
2017-07-05 22:06:57 +0800

    springboot websocket example

---
+++ springboot-demo-websocket/src/main/java/springlearning/SpringbootDemoWebsocketApplication.java
@@ -0,0 +1,12 @@
+package springlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoWebsocketApplication.class, args);
+	}
+}
