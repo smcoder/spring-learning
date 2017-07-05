e2add2d538ed724182b1750f2fcc9f22fa3c1678
mac <mac@mac.local>
2017-07-05 09:46:57 +0800

    Spring bean的作用域例子

---
+++ spring-demo-4/src/main/java/com/personal/ScopeTest.java
@@ -0,0 +1,14 @@
+package com.personal;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by mac on 05/07/2017.
+ */
+@Component
+@Scope("singleton")
+//@Scope("prototype")
+public class ScopeTest {
+
+}
